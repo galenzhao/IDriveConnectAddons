fdaa7c5b219b8503a23b090b055b8fabb71b9abf
Galen Zhao <github@zhenggangzhao.org>
2023-05-12 17:53:53 +0800

    forgot to restore the default dimens

--- screen_mirror/src/main/java/io/bimmergestalt/idriveconnectaddons/screenmirror/CustomRHMIDimensions.kt
+++ screen_mirror/src/main/java/io/bimmergestalt/idriveconnectaddons/screenmirror/CustomRHMIDimensions.kt
@@ -4,17 +4,17 @@ import io.bimmergestalt.idriveconnectkit.RHMIDimensions
 
 class CustomRHMIDimensions(val original: RHMIDimensions, val settingsViewer: AppSettingsViewer): RHMIDimensions {
 	override val rhmiWidth: Int
-		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_RHMI_WIDTH].toIntOrNull() ?: 980
+		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_RHMI_WIDTH].toIntOrNull() ?: original.rhmiWidth
 	override val rhmiHeight: Int
-		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_RHMI_HEIGHT].toIntOrNull() ?: 540
+		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_RHMI_HEIGHT].toIntOrNull() ?: original.rhmiHeight
 	override val marginLeft: Int
-		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_MARGIN_LEFT].toIntOrNull() ?: 90
+		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_MARGIN_LEFT].toIntOrNull() ?: original.marginLeft
 	override val paddingLeft: Int
-		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_PADDING_LEFT].toIntOrNull() ?: 90
+		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_PADDING_LEFT].toIntOrNull() ?: original.paddingLeft
 	override val paddingTop: Int
-		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_PADDING_TOP].toIntOrNull() ?: 67
+		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_PADDING_TOP].toIntOrNull() ?: original.paddingTop
 	override val marginRight: Int
-		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_MARGIN_RIGHT].toIntOrNull() ?: 5
+		get() = settingsViewer[AppSettings.KEYS.DIMENSIONS_MARGIN_RIGHT].toIntOrNull() ?: original.marginRight
 }
 class UpdatingSidebarRHMIDimensions(val fullscreen: RHMIDimensions, val isWidescreen: () -> Boolean):
 		RHMIDimensions {
