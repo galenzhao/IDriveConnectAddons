7a7b296f96d640a5501dbb750ca7e7b6d885d86e
Galen Zhao <github@zhenggangzhao.org>
2023-09-20 15:02:55 +0800

    merge auto permission

--- screen_mirror/src/main/java/io/bimmergestalt/idriveconnectaddons/screenmirror/MainActivity.kt
+++ screen_mirror/src/main/java/io/bimmergestalt/idriveconnectaddons/screenmirror/MainActivity.kt
@@ -25,8 +25,17 @@ import android.provider.Settings.canDrawOverlays
 import androidx.activity.viewModels
 import io.bimmergestalt.idriveconnectaddons.screenmirror.databinding.ActivityMainBinding
 import androidx.annotation.Nullable;
-
-
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 const val TAG = "ScreenMirroring"
 
@@ -36,6 +45,31 @@ class MainActivity : AppCompatActivity() {
 
     private val REQUEST_PERMISSION_CODE = 1
 
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private val notificationManager: NotificationManager by lazy {
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    }
+    companion object {
+        const val CHANNEL_ID = "dummy_channel"
+    }
+    /**
+     * Shows a notification to user.
+     *
+     * The notification won't appear if the user doesn't grant notification permission first.
+     */
+    private fun showDummyNotification() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Congratulations! 🎉🎉🎉")
+            .setContentText("You have post a notification to Android 13!!!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(1, builder.build())
+        }
+    }
+
+    //@RequiresApi(33)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppSettings.loadSettings(applicationContext)
@@ -96,18 +130,59 @@ class MainActivity : AppCompatActivity() {
                 requestPermission()
             }
         }
+        // Sets up notification channel.
+        createNotificationChannel()
+
+        // Sets up permissions request launcher.
+        requestPermissionLauncher = registerForActivityResult(RequestPermission()) {
+//            refreshUI()
+            if (it) {
+                showDummyNotification()
+            } else {
+//                Snackbar.make(
+//                    findViewById<View>(android.R.id.content).rootView,
+//                    "Please grant Notification permission from App Settings",
+//                    Snackbar.LENGTH_LONG
+//                ).show()
+            }
+        }
     }
 
+    /**
+     * Creates Notification Channel (required for API level >= 26) before sending any notification.
+     */
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Important Notification Channel",
+                NotificationManager.IMPORTANCE_HIGH,
+            ).apply {
+                description = "This notification contains important announcement, etc."
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
     override fun onResume() {
         super.onResume()
 
         if(canDrawOverlays(this)){
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    "android.permission.POST_NOTIFICATIONS",
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                showDummyNotification()
+            } else {
+                requestPermissionLauncher.launch("android.permission.POST_NOTIFICATIONS")
+            }
             if(getString(R.string.lbl_status_not_allowed).equals(findViewById<TextView>(R.id.mirroringStateText).text.toString(),true)) {
                 println("PROJECT_MEDIA permission: ${checkSelfPermission("PROJECT_MEDIA")}")
                 AppSettings.loadSettings(applicationContext)
 
                 val settingsViewer = AppSettingsViewer()
-                if (settingsViewer[AppSettings.KEYS.AUTO_PERMISSION].isNotEmpty() && settingsViewer[AppSettings.KEYS.AUTO_PERMISSION].toInt() > 0) {
+                if (settingsViewer[AppSettings.KEYS.AUTO_PERMISSION].isNotEmpty() && settingsViewer[AppSettings.KEYS.AUTO_PERMISSION].toInt() > 100) {
                     val appOps = getSystemService(AppOpsManager::class.java)
                     println("PROJECT_MEDIA appops: ${appOps.checkOpNoThrow("android:project_media", Process.myUid(), packageName)}")
                     controller.promptPermission(false)
